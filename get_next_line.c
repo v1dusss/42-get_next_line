9eb4ee7deaa06f80948bcec53663e4cfb06eff13
v1dusss <vsivanat@student.42heilbronn.de>
2024-12-19 18:51:23 +0100

    moved it in to one file

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,53 @@
 /*   By: vsivanat <vsivanat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 12:22:51 by vsivanat          #+#    #+#             */
-/*   Updated: 2023/12/12 20:13:19 by vsivanat         ###   ########.fr       */
+/*   Updated: 2024/12/19 18:49:48 by vsivanat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int	ft_strlen(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+		return (-1);
+	while (str[i] && str[i] != c)
+		i++;
+	if (str[i] == c)
+		return (i);
+	return (-1);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		a;
+	char	*result;
+
+	if (!s2)
+		return (NULL);
+	if (!s1 || s1 == NULL)
+	{
+		s1 = malloc(sizeof(char) * 1);
+		if (!s1)
+			return (NULL);
+		s1[0] = 0;
+	}
+	result = malloc((ft_strlen(s1, '\0') + ft_strlen(s2, '\0') + 1) * sizeof(char));
+	if (!result)
+		return (free(s1), NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		result[i] = s1[i];
+	a = 0;
+	while (s2[a] != '\0')
+		result[i++] = s2[a++];
+	result[i] = '\0';
+	return (free(s1), result);
+}
 
 char	*ft_trim_temp(char *temp, char **cache)
 {
@@ -86,31 +128,3 @@ char	*get_next_line(int fd)
 	cache = next(cache);
 	return (ft_trim_temp(temp, &cache));
 }
-
-// # include <unistd.h>
-// # include <stdlib.h>
-// # include <stdio.h>
-// #include <fcntl.h>
-// int	main()
-// {
-// 	int	fd;
-
-// 	fd = open("test.txt", O_RDWR);
-// 	if (fd == -1)
-// 	{
-// 		// perror("Error opening file");
-// 		return 1;
-// 	}
-// 	// printf("%d", fd);
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	printf("%s", get_next_line(fd));
-// 	close(fd);
-// 	return (0);
-// }
